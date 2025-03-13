58c71b45945ee92f8a8889c39b36e5fc5e2bd602
Ma <mazou.marine@gmail.com>
2025-03-13 10:28:55 +0100

    Refactor navigation structure and add legal mentions page with styling

--- index.php
+++ index.php
@@ -7,31 +7,30 @@
 </head>
 
 <body>
+    <header>
+        <nav class="navbar">
+            <div class="logo">LOGO</div>
 
-    <nav class="navbar">
-        <div class="logo">LOGO</div>
-        
-        <div class="home">
-            <img src="assets/icons/home.svg" alt="">
-        </div>
-
-        <div class="search-bar">
-            <img src="assets/icons/menu-burger.svg" alt="">
-            <img src="assets/icons/search.svg" alt="">
-            <input type="text" placeholder="Hinted search text">
-        </div>
-
-        <div class="user">
-            <img src="assets/icons/user.svg" alt="">
-            <div>
-                <h1>NOM</h1>
-                <p>role</p>
+            <div class="home">
+                <img src="assets/icons/home.svg" alt="">
             </div>
-        </div>
 
-    </nav>
+            <div class="search-bar">
+                <img src="assets/icons/menu-burger.svg" alt="">
+                <img src="assets/icons/search.svg" alt="">
+                <input type="text" placeholder="Hinted search text">
+            </div>
 
-    </head>
+            <div class="user">
+                <img src="assets/icons/user.svg" alt="">
+                <div>
+                    <h1>NOM</h1>
+                    <p>role</p>
+                </div>
+            </div>
+        </nav>
+    </header>
+
     <main>
 
     </main>
--- vues/Publisher.php
+++ vues/Publisher.php
@@ -2,32 +2,75 @@
 <html>
 
 <head>
-    <title>Internity - Publisher</title>
+    <title>Internity - Mentions Légales</title>
     <link rel="stylesheet" href="/assets/styles.css">
+    <link rel="stylesheet" href="/assets/publisher.css">
 </head>
 
 <body>
-    <nav class="navbar">
-        <div class="logo">LOGO</div>
+    <header>
+        <nav class="navbar">
+            <div class="logo">LOGO</div>
 
-        <i class="fi fi-rs-home"></i> <!-- home icon -->
+            <div class="home">
+                <img src="/assets/icons/home.svg" alt="">
+            </div>
 
-        <div class="search-bar">
+            <div class="search-bar">
+                <img src="/assets/icons/menu-burger.svg" alt="">
+                <img src="/assets/icons/search.svg" alt="">
+                <input type="text" placeholder="Hinted search text">
+            </div>
 
-            <i class="fi fi-br-menu-burger"></i> <!-- burger icon -->
-            <input type="text" placeholder="Hinted search text">
-            <i class="fi fi-rr-search"></i> <!-- search icon -->
-        </div>
+            <div class="user">
+                <img src="/assets/icons/user.svg" alt="">
+                <div>
+                    <h1>NOM</h1>
+                    <p>role</p>
+                </div>
+            </div>
+        </nav>
+    </header>
 
-        <div>
-            <i class="fi fi-rs-circle-user"></i> <!-- user icon -->
-        </div>
-    </nav>
+    <main class="publisher">
+        <p>Site enregistré sous le n° </p>
 
-    </head>
-    <main>
-        <p>PUTE</p>
+        <section>
+            <h2>Éditeur</h2>
+            <p>Internity</p>
+            <p>27 Avenue de la Plage</p>
+            <p>64200 Biarritz, France</p>
+            <p>Tél : +33(0)x xx xx xx xx</p>
+            <p>Tél : +33(0)x xx xx xx xx</p>
+            <p>Directeur de la publication : Marine Mazou</p>
+            <p>Pour contacter les webmaster : contact@internity.com</p>
+        </section>
+
+        <section>
+            <h2>Hébergeur</h2>
+            <p>Hébergement interne</p>
+            <p>27 Avenue de la Plage</p>
+            <p>64200 Biarritz, France</p>
+            <p>Tél : +33(0)x xx xx xx xx</p>
+        </section>
+
+        <section>
+            <h2>Utilisation des données et Copyright</h2>
+            <p>Les données collectées sur ce site sont exclusivement utilisées dans le cadre de l'optimisation de nos services, de la proposition d'offres de stage personnalisées aux 
+            étudiants, ainsi que pour réaliser des analyses statistiques visant à améliorer l'expérience utilisateur et la pertinence des opportunités proposées.</p>
+            <p>Aucune donnée personnelle n'est vendue, échangée ou cédée à des tiers sans consentement explicite.</p>
+            <p>Conformément au Règlement Général sur la Protection des Données (RGPD), vous disposez d’un droit d’accès, de rectification et de suppression de vos données.
+            Pour exercer ces droits, contactez-nous à contact@internity.com.</p>
+        </section>
+
+        <section>
+            <h2>Cookies</h2>
+            <p>Ce site utilise des cookies pour améliorer l'expérience utilisateur.</p>
+        </section>
+
     </main>
+
+    <br>
 
     <footer>
         <a href="/vues/publisher.php">Mention l'égale</a>
