738c5f01ad95b4c8e18b0d4556f6fce839f10b56
mxriine <mazou.marine@gmail.com>
2025-03-25 14:18:18 +0100

    Update database password for improved security

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -3,6 +3,6 @@
 define("DB_HOST", "localhost");
 define("DB_NAME", "Internity");
 define("DB_USER", "Internity");
-define("DB_PASS", "in/ternityxx25");
+define("DB_PASS", "In/TernityXX25!");
 define("BASE_URL", "http://localhost:8000/");
 define("DB_CHARSET", "utf8");
