8216da08f0fe8a0d72cc8856e6162d833112d3db
Ma <79311390+mxriine@users.noreply.github.com>
2025-04-04 10:29:28 +0200

    Update Config.php

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -1,8 +1,1 @@
-<?php
 
-define("DB_HOST", "localhost");
-define("DB_NAME", "Internity");
-define("DB_USER", "Internity");
-define("DB_PASS", "In/TernityXX25!");
-define("BASE_URL", "http://localhost:8000/");
-define("DB_CHARSET", "utf8");
